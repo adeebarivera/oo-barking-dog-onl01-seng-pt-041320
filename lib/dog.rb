24e9500e5fde2db3f4e756570f8414f870443f18
adeeba reaz <adeebarivera@outlook.com>
2020-04-27 18:09:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,18 @@
 class Dog
- 
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
+    @name = dogs_name
+  end 
+
   def name
-    @this_dogs_name
-  end
-end
- 
-lassie = Dog.new
-lassie.name = "Lassie"
- 
-puts lassie.name
+    @name
+  end 
+  
+end 
+
+class Dog
+  def bark
+    puts "woof!"
+  end 
+  
+end 
  
